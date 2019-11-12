05fd00dd01a903839df13d20473c4ea7773d10cb
Josh Bendavid <Josh.Bendavid@cern.ch>
2019-11-12 11:44:00 +0100

    code formatting

--- xbmc/interfaces/json-rpc/InputOperations.h
+++ xbmc/interfaces/json-rpc/InputOperations.h
@@ -19,7 +19,12 @@ namespace JSONRPC
   public:
     static JSONRPC_STATUS SendText(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
     static JSONRPC_STATUS ExecuteAction(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
-    static JSONRPC_STATUS ButtonEvent(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
+
+    static JSONRPC_STATUS ButtonEvent(const std::string& method,
+                                      ITransportLayer* transport,
+                                      IClient* client,
+                                      const CVariant& parameterObject,
+                                      CVariant& result);
 
     static JSONRPC_STATUS Left(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
     static JSONRPC_STATUS Right(const std::string &method, ITransportLayer *transport, IClient *client, const CVariant &parameterObject, CVariant &result);
